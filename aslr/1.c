5e2309f0028d224ce55a1c14a5a5874987d8b293
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-10-30 05:39:28 +0100

    add aslr

---
+++ aslr/1.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+typedef struct {
+  char data1;
+  short data2;
+  // struct has a short (2-byte) value at offset 1, making it unaligned
+  // causing compilers to do padding
+  // but padding bytes are uninitialized and get random values from ASLR
+} unalignedstruct;
+
+main()
+{
+  unalignedstruct x;
+  x.data1=1;
+  x.data2=0x203;
+  write(1, &x, sizeof(x));
+  return 0;
+}
---
+++ aslr/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main()
+{
+  int x;
+  printf("%p\n", &x);
+  return 0;
+}

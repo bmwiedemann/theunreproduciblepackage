5d120c4993c8aca6c5202eb05c987ff7f2b20860
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-10-30 05:24:58 +0100

    readdir: add os.walk note

--- readdir/readdir.py
+++ readdir/readdir.py
@@ -9,4 +9,5 @@ for entry in glob.glob('in/*'):
     print(entry);
 
 for entry in os.walk('in'):
+    # note: for nested dirs, both list of dirs and list of files need to be sorted to make the output reproducible
     print(entry);

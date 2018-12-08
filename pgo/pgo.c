35e1af77f98dad839ce392c0cbaff8236e037672
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-12-08 23:32:17 +0100

    pgo: simplify the example
    do not require mkpasswd
    and even increase the number of possible build results,
    because now we have variations in input length.
    
    Also we are explicitly calling bash
    because dash does not know about $RANDOM

--- pgo/pgo.c
+++ pgo/pgo.c
@@ -5,7 +5,7 @@ int main()
   char ret=0;
   char c;
   while(read(0, &c, 1) == 1) {
-    if(c < 'm') ret++; // count the letters below "m"
+    if(c < '4') ret++; // count the chars below "4"
   }
   return ret;
 }

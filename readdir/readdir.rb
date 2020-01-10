3a76e4056b4c945b61554b74a374d39fcf2dc2f2
Bernhard M. Wiedemann <bwiedemann@suse.de>
2020-01-10 09:59:33 +0100

    readdir: add ruby glob variant

--- readdir/readdir.rb
+++ readdir/readdir.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/ruby
 puts Dir.glob("in/*")
+puts Dir["in/*"]
 puts Dir.children("in/")
 puts Dir.entries("in/")
 # See also: .foreach .each .each_child .read
 
 # Find with:
-# egrep -r 'Dir\.(glob|children|entries|foreach|each|read)' .
+# egrep -r -e 'Dir\.(glob|children|entries|foreach|each|read)' -e 'Dir\[' .
 
 # Fix with:
 # puts Dir.entries("in/").sort

bb91b34ca28ec8a7e72b57b72148bd3acb3455b8
Bernhard M. Wiedemann <bwiedemann@suse.de>
2021-02-05 13:26:25 +0100

    Switch to python3
    because python2 is obsolete and missing in some places

--- hash/hash.py
+++ hash/hash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # works with python2 and python3
 
 myhash={}
--- timestamp/genversion.py
+++ timestamp/genversion.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import datetime
 print('BUILD_DATE="'+datetime.datetime.today().strftime("%F %T") + "\"")

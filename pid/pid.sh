1434b7809249b091a07011e4c2806e980cc694ed
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-04-25 08:11:57 +0200

    pid: Add tar example

--- pid/pid.sh
+++ pid/pid.sh
@@ -3,3 +3,6 @@ tmp=pidtmpfilename$$.c
 cp -a dummy.c $tmp
 gcc $tmp -o ../out/pidfromgcc
 rm -f $tmp
+
+# https://reproducible-builds.org/docs/archives/
+tar c pid.sh > ../out/pidfrom.tar # POSIX PaX headers have a PID by default

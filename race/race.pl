23212e0feec9a653cef3ebb5c1cba1819dc030d3
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-04-25 15:59:08 +0200

    race: capture perl output
    so we can diff it as part of the package

--- race/race.pl
+++ race/race.pl
@@ -2,9 +2,11 @@
 # this behaves more deterministic than bash examples
 # but with varying taskset $N, it also has some randomness
 use strict;
+open (OUT, ">", "../out/race.pl.out");
 for(1..5) {
   my $pid = fork();
   if($pid==0){
+    print OUT "$_\n";
     print "$_\n";
     exit 0
   }
